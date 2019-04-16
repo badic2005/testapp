7ab780b12bd055692b6e74427b593ff633dbede1
badic <vadim021703@gmail.com>
2019-04-16 11:17:24 +0300

    fix

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 
+var favicon = require('serve-favicon')
+var path = require('path')
+
+require("dotenv").config();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-app.listen('8000', function () {
+const port = process.env.PORT  || '8000';
+app.listen(port, function () {
     console.log('listen 8000')
 })
